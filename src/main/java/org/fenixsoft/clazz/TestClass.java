2da37f7ecfe7b857422a9f0a45ffea010b72c501
lanboys <lan_bing2013@163.com>
2019-12-12 23:56:28 +0800

    初始化项目

---
+++ src/main/java/org/fenixsoft/clazz/TestClass.java
@@ -0,0 +1,20 @@
+package org.fenixsoft.clazz;
+
+/**
+ * Created by 蓝兵 on 2019/11/29.
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+
+}
+
+
+
+
+
+
