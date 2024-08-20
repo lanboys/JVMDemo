72143b8998f5bc3f1007f09fae0637614d61bdf3
lanboys <lan_bing2013@163.com>
2024-08-20 22:26:04 +0800

    部分测试文件

--- src/main/java/org/fenixsoft/jvm/chapter2/HeapOOM.java
+++ src/main/java/org/fenixsoft/jvm/chapter2/HeapOOM.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * VM Args：-Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError
  *
  * @author zzm
  */
@@ -12,14 +12,18 @@ public class HeapOOM {
 
     static class OOMObject {
 
-        double lan = 1500000.0;
+        double lan = 150.0;
+        // double bing = 350.0;
+        // double lan = 1500000.0;// 大小效果是一样的，除非加多几个字段
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<OOMObject> list = new ArrayList<OOMObject>();
 
         while (true) {
             System.out.println("main(): " + list.size());
+
+            // Thread.sleep(2000);
 
             list.add(new OOMObject());
         }
